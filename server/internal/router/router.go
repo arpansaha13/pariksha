507f679f1eaf16e9dc2b44af4f366bbbd654671d
arpansaha13 <arpansaha1300@gmail.com>
2025-01-25 20:43:36 +0530

    refactor: rename controllers to handlers

--- server/internal/router/router.go
+++ server/internal/router/router.go
@@ -3,16 +3,16 @@ package router
 import (
 	"github.com/gorilla/mux"
 
-	"github.com/arpansaha13/pariksha/internal/controllers"
+	"github.com/arpansaha13/pariksha/internal/handlers"
 )
 
 func SetupRouter() *mux.Router {
 	r := mux.NewRouter()
 
 	// Auth Routes
-	r.HandleFunc("/auth/login", controllers.Login).Methods("POST")
-	r.HandleFunc("/auth/signup", controllers.SignUp).Methods("POST")
-	r.HandleFunc("/auth/verification/{hash}", controllers.Verification).Methods("POST")
+	r.HandleFunc("/auth/login", handlers.Login).Methods("POST")
+	r.HandleFunc("/auth/signup", handlers.SignUp).Methods("POST")
+	r.HandleFunc("/auth/verification/{hash}", handlers.Verification).Methods("POST")
 
 	return r
 }
